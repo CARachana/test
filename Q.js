bb99a7b65654bfe2f71e474e02006813088bf729
CARachana <126049214+CARachana@users.noreply.github.com>
2023-02-28 15:49:54 +0530

    Create Q.js

---
+++ Q.js
@@ -0,0 +1,206 @@
+
+const FA =[  
+	{
+		q:"Which of these is an irrelevant ratio for a bank?",
+		options:["ROCE", "Debt to Equity Ratio", "Inventory Turnover Ratio", "All of the above"],
+		answer:3
+	},
+	{
+		q:"Which of the following is included in the Operating income of a bank?",
+		options:["Interest earned on loans and advances" , "Interest earned on deposits with other banks", "Interest given to deposit holders", "Salary to staff"],
+		answer:0
+	},
+	{
+		q:"Identify the false statement about NPAs of banks.",
+		options:["NPAs are loans given by the bank that the bank is unable to recover", "Banks are required to make provisions in respect of NPAs", "NPAs are expressed as a % of total loans given", "NPAs are higher the better."],
+		answer:3
+	},
+	{
+		q:"It is normal to have a negative cash flow in operating activities of a company.",
+		options:["TRUE", "FALSE", "True, except for Banks and NBFCs", "False, except for Banks and NBFCs"],
+		answer:3
+	},
+	{
+		q:"Identify the false statement",
+		options:["Share capital can be negative", "Total shareholders' funds includes Share capital+Reserves", "Total shareholders' funds includes Share capital+Reserves", "Shareholders' funds does not include borrowings"],
+		answer:0
+	}
+];
+
+		
+		const TA =[  
+
+	{
+		q:"Markets have which of the following phases?",
+		options:[" accumulation phase" , "public participation phase", "distribution phase", "all of the above"],
+		answer:3
+	},
+	{
+		q:"A line chart is made by plotting the ___ prices.",
+		options:["open", "high", "close", "low"],
+		answer:2
+	},
+	{
+		q:"Technical analysis consists of:",
+		options:["Analysing the financial statements", "Analysing the macro-economic factors", "Analysing the company’s quarterly results", "None of the above"],
+		answer:3
+	},
+	{
+		q:"Lower highs and lower lows are formed in a:",
+		options:["uptrend", "downtrend", "sideways trend", "none of the above"],
+		answer:1
+	},
+	{
+		q:"In a downtrend, if we see if big red candle followed by a small green candle inside the body of the red candle, what is the candle pattern called?",
+		options:["bullish harami", "bearish engulfing", "bearish harami", "bullish engulfing"],
+		answer:0
+	}
+];
+
+		
+		const FNO =[  
+
+	{
+		q:"Which of the following is the Regulatory authority for currency derivatives",
+		options:[" SEBI" , "RBI", "NSE", "BSE"],
+		answer:1
+	},
+	{
+		q:"Which of the following is not a type of derivative",
+		options:["Forwards", "Futures", "Swaps", "Bonds"],
+		answer:3
+	},
+	{
+		q:"Which of the following is not a condition to be a part of Nifty 50 Index",
+		options:["It should be an Indian Company (Company should be registered in India)", "It should be a part of F&O", "The company’s shares should be illiqiuid", "None of the above"],
+		answer:2
+	},
+	{
+		q:"If the lot size is 25 and a person wants to hedge downside for 300 shares that she has with her, how many lot should she short?",
+		options:["25", "300", "12", "7500"],
+		answer:2
+	},
+	{
+		q:"What will be the Leverage ratio in case the margin requirement is 40%",
+		options:["4", "2.5", "40", "can not be calculated with given information"],
+		answer:1
+	}
+];
+
+		
+		const BOSM =[  
+		
+	{
+		q:"Which of the following stock exchange is in India?",
+		options:[" BSE" , "RBI", "NYSE", "TSX"],
+		answer:0
+	},
+	{
+		q:"Which of the following is not a participant in the stock exchange?",
+		options:["Regulator", "Broker", "Clearing House", "Insurance Company"],
+		answer:3
+	},
+	{
+		q:"Which of the following is not a type of market?",
+		options:["Secondary Market", "Primary Market", "Tertiary Market", "None of the above"],
+		answer:2
+	},
+	{
+		q:"How many stock exchanges are present in India?",
+		options:["1", "7", "3", "4"],
+		answer:1
+	},
+	{
+		q:"Which of the following is a stage in the Life Cycle of a company?",
+		options:["Series A Funding", "Series B Funding", "IPO", "All of the Above"],
+		answer:3
+	}
+];
+
+			
+			const MF =[  
+
+	{
+		q:"What is a Mutual Fund?",
+		options:["Number of investors who share a common investment objective pool in their money with an intention of earning returns" , "Number of investors who pool in their money with an intention of funding a company’s project", "It is a company that owns , operates, or finances income producing properties", "Non of the above"],
+		answer:0
+	},
+	{
+		q:"Which was the first Mutual fund entity established in India?",
+		options:["Kothari Pioneer Mutual Fund", "Unit Trust of India", "SBI Mutual Fund", "Infosys Mutual Fund"],
+		answer:1
+	},
+	{
+		q:"Kothari Pioneer Mutual Fund is now merged with which AMC?",
+		options:["Franklin Templeton Mutual Fund", "DSP Mutual Fund", "IDFC Mutual Fund", "None of the above"],
+		answer:0
+	},
+	{
+		q:"Who maintains the records for Mutual Fund investors accounts?",
+		options:["Custodian", "Registrar & Transfer Agent", "Sponsor", "None of the above"],
+		answer:1
+	},
+	{
+		q:"Who accounts for Dividends received on Mutual Fund investments?",
+		options:["Custodian", "Registrar & Transfer Agent", "Asset Management Company", "All of the Above"],
+		answer:0
+	}
+];
+
+	
+	const MMM =[  
+
+	{
+		q:"What is the first step in Money Management?",
+		options:["Invest in Shares" , "Get yourself insured", "Create Emergency Fund", "Buy Gold"],
+		answer:2
+	},
+	{
+		q:"It is all right if a housewife does not have medical insurance. True or False?",
+		options:["True", "False"],
+		answer:1
+	},
+	{
+		q:"What is the full form of PMJJBY?",
+		options:["Pradhan Mantri Jal Jeevan Banaras Yojana", "Pradhan Mantri Jeevan Jyoti Bulb Yojana", "Pradhan Mantri Jeevan Jyoti Bima Yojana", "Pradhan Mantri Jagat Janani Bikas Yojana"],
+		answer:2
+	},
+	{
+		q:"How much Emergency Fund is sufficient?",
+		options:["6 times your annual income", "10 times your annual income", "6 times your monthly income", "16 times your monthly income"],
+		answer:2
+	},
+	{
+		q:"The premium of Rs. 20 for PMSBY is",
+		options:["Monthly", "Weekly", "Annually", "Daily"],
+		answer:2
+	}
+	];
+	 
+	const VA =[
+		{
+			q:"Valuation is done _____ fundamental analysis",
+			options:["Before" , "After", "Alongside", "None of the above"],
+			answer:1
+		},
+		{
+			q:"There are how many approaches to valuation?",
+			options:["1", "2" , "3", "4"],
+			answer:2
+		},
+		{
+			q:"Valuation using peer data comes under ____",
+			options:["Asset approach", "Income approach", "Relative valuation", "None of the above"],
+			answer:2
+		},
+		{
+			q:"Under relative valuation there are 2 types Comparable Company Multiple and ____",
+			options:["Compatible transaction multiple", "Comparable transition multiple", "Comparable transaction multiple", "Compatible transition multiple"],
+			answer:2
+		},
+		{
+			q:"Asset approach is also known as the ____",
+			options:["Net annual value approach", "Net actual value approach", "Net asset value approach", "Net value approch"],
+			answer:2
+		},
+		];
